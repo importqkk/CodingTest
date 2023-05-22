6f46b6068394ae43481ca6fc7238b87b1a24625c
importqkk <import_qkk@naver.com>
2023-05-22 22:46:41 +0900

    230522

--- CodingTest/src/baekjoon/q11720/Main2.java
+++ CodingTest/src/baekjoon/q11720/Main2.java
@@ -1,10 +1,9 @@
 package baekjoon.q11720;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main2 {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +14,7 @@ public class Main {
 		
 		int sum = 0;
 		for(int i=0; i<n; i++) {
-			sum += Integer.parseInt(String.valueOf(num.charAt(i)));
+			sum += Character.getNumericValue(num.charAt(i));
 		}
 		
 		System.out.println(sum);
