d6c9e56f909ff1224927ddfb951890ccaa68e43d
importqkk <import_qkk@naver.com>
2023-05-25 23:30:20 +0900

    230525

--- CodingTest/src/baekjoon/q11718/Main.java
+++ CodingTest/src/baekjoon/q11718/Main.java
@@ -7,13 +7,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		String str;
 		
 		while((str = br.readLine()) != null) {
-			System.out.println(str);
+			sb.append(str).append("\n");
 		}
-		
 		br.close();
+		
+		System.out.println(sb.toString());
 		
 	}
 }
