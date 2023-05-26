ad04c3b03fdb2bce3bb5aa2a02d811c96c0e7fee
importqkk <import_qkk@naver.com>
2023-05-26 20:43:49 +0900

    230526

---
+++ CodingTest/src/baekjoon/q25083/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.q25083;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7").append("\n")
+			.append("r`-_   ,'  ,/").append("\n")
+			.append(" \\. \". L_r'").append("\n")
+			.append("   `~\\/").append("\n")
+			.append("      |").append("\n")
+			.append("      |").append("\n");
+		System.out.println(sb.toString());
+		
+	}
+}
---
+++ CodingTest/src/baekjoon/q25083/Main2.java
@@ -0,0 +1,14 @@
+package baekjoon.q25083;
+
+public class Main2 {
+	public static void main(String[] args) {
+		
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+}
