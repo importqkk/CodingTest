a8353cc89f2def3bc1bd411d5820daa9ad99e919
importqkk <import_qkk@naver.com>
2023-06-08 22:07:54 +0900

    230608

---
+++ CodingTest/src/baekjoon/q2745/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.q2745;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		String n = st.nextToken();
+		int b = Integer.parseInt(st.nextToken());
+		
+		long ans = 0;
+		int sq = 0;
+		int num = 0;
+		
+		for(int i = n.length()-1; i >= 0; i--) {
+			char c = n.charAt(i);
+			if(c >= '0' && c <= '9') num = c;
+			else num = c - 55;
+			ans += num * Math.pow(b, sq++);
+		}
+		
+		br.close();
+		
+		System.out.println(ans);
+		
+	}
+}
