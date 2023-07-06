48cece6f78db7e1d0a41623ec8c0082b0f449726
importqkk <import_qkk@naver.com>
2023-07-06 23:35:08 +0900

    230706

---
+++ CodingTest/src/baekjoon/q2798/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.q2798;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		br.close();
+		
+		int max = 0;
+		
+		for(int i=0; i<n-2; i++) {
+			for(int j=i+1; j<n-1; j++) {
+				for(int k=j+1; k<n; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					if(sum == m) {
+						max = sum;
+						break;
+					}
+					if(sum > max && sum < m) max = sum;
+				}
+			}
+		}
+		
+		System.out.println(max);
+		
+	}
+}
