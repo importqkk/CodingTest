48efcf95a2d0ce890696425dc82b569fb9ffa844
importqkk <import_qkk@naver.com>
2023-07-11 15:54:55 +0900

    230711

---
+++ CodingTest/src/baekjoon/q2839/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.q2839;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		
+		int cnt = 0;
+		
+		while(true) {
+			if(n%5 == 0) {
+				cnt -= n/5; break;
+			}
+			else {
+				n -= 3; cnt++;
+			}
+			if(cnt < 0) {
+				cnt = -1; break;
+			}
+		}
+		
+		System.out.println(cnt);
+		
+	}
+}
