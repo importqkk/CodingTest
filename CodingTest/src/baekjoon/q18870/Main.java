474007e835cd8400270cf315905b7f954893d50c
importqkk <import_qkk@naver.com>
2023-07-22 18:02:52 +0900

    210722

---
+++ CodingTest/src/baekjoon/q18870/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.q18870;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] sort = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = sort[i] = Integer.parseInt(st.nextToken());
+		}
+		br.close();
+		
+		Arrays.sort(sort);
+		Map<Integer, Integer> map = new HashMap<>();
+		int order = 0;
+		for(int i=0; i<n; i++) {
+			if(!map.containsKey(sort[i])) {
+				map.put(sort[i], order); order++;
+			}
+		}
+		
+		System.out.println(map);
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<n; i++) {
+			sb.append(map.get(arr[i])).append(" ");
+		}
+		System.out.println(sb);
+		
+	}
+}
---
+++ CodingTest/src/baekjoon/q18870/Main2.java
@@ -0,0 +1,33 @@
+package baekjoon.q18870;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+// 시간초과
+public class Main2 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		br.close();
+		
+		List<Integer> sort = list.stream().distinct().collect(Collectors.toList());
+		Collections.sort(sort);
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<list.size(); i++) {
+			sb.append(sort.indexOf(list.get(i))).append(" ");
+		}
+		System.out.println(sb);
+		
+	}
+}
