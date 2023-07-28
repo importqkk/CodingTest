e80b005b2e4663d805a54c6860a2c36adc0b201b
importqkk <import_qkk@naver.com>
2023-07-28 23:48:53 +0900

    230728

---
+++ CodingTest/src/baekjoon/q11478/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.q11478;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		br.close();
+		
+		Set<String> set = new HashSet<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			for(int j=i+1; j<=s.length(); j++) {
+				set.add(s.substring(i, j));
+			}
+		}
+		
+		System.out.println(set.size());
+		
+	}
+}
