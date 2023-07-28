617a021db7d9a5282cfb8ea310fb10dba34bf30a
importqkk <import_qkk@naver.com>
2023-07-29 00:26:35 +0900

    230729

---
+++ CodingTest/src/baekjoon/q1934/Main.java
@@ -0,0 +1,37 @@
+package baekjoon.q1934;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int d = gcd(a, b);
+			sb.append(a * b / d).append("\n");
+		}
+		br.close();
+		
+		System.out.println(sb.toString());
+		
+	}
+
+	private static int gcd(int a, int b) {
+		while(b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
