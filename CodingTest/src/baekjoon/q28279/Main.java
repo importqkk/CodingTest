ff692a2a8221bc5ae01190e8998ddc65d99c512a
importqkk <import_qkk@naver.com>
2023-08-15 15:48:05 +0900

    230815

---
+++ CodingTest/src/baekjoon/q28279/Main.java
@@ -0,0 +1,54 @@
+package baekjoon.q28279;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		Deque<Integer> d = new LinkedList<>();
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int order = Integer.parseInt(st.nextToken());
+			
+			switch(order) {
+				case 1: {
+					d.addFirst(Integer.parseInt(st.nextToken())); break;
+				}
+				case 2: {
+					d.addLast(Integer.parseInt(st.nextToken())); break;
+				}
+				case 3: {
+					sb.append(d.isEmpty() ? -1 : d.pollFirst()).append("\n"); break;
+				}
+				case 4: {
+					sb.append(d.isEmpty() ? -1 : d.pollLast()).append("\n"); break;
+				}
+				case 5: {
+					sb.append(d.size()).append("\n"); break;
+				}
+				case 6: {
+					sb.append(d.isEmpty() ? 1 : 0).append("\n"); break;
+				}
+				case 7: {
+					sb.append(d.isEmpty() ? -1 : d.peekFirst()).append("\n"); break;
+				}
+				case 8: {
+					sb.append(d.isEmpty() ? -1 : d.peekLast()).append("\n"); break;
+				}
+			}
+		}
+		br.close();
+		
+		System.out.println(sb.toString());
+		
+	}
+}
