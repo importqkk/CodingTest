305a9be434ced83423f1ba161be9c9cef869d8e5
importqkk <import_qkk@naver.com>
2023-08-25 18:16:07 +0900

    230825

---
+++ CodingTest/src/baekjoon/q26069/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.q26069;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		Set<String> set = new HashSet<>();
+		set.add("ChongChong");
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String name1 = st.nextToken();
+			String name2 = st.nextToken();
+			if(set.contains(name1) || set.contains(name2)) {
+				set.add(name1);
+				set.add(name2);
+			}
+		}
+		br.close();
+		
+		System.out.println(set.size());
+		
+	}
+}
