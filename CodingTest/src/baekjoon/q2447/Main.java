0f9c07c38f1a32872b86d5e69dc24dc050b23aca
importqkk <import_qkk@naver.com>
2023-09-03 21:27:41 +0900

    230902

--- CodingTest/src/baekjoon/q2447/Main.java
+++ CodingTest/src/baekjoon/q2447/Main.java
@@ -1,9 +1,50 @@
 package baekjoon.q2447;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+// https://st-lab.tistory.com/95
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		arr = new char[N][N];
+        
+		star(0, 0, N, false);
+ 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) sb.append(arr[i][j]);
+			sb.append('\n');
+		}
+		System.out.print(sb);
 		
-		
+	}
+ 
+	static char[][] arr;
+	static void star(int x, int y, int N, boolean blank) {
+		if (blank) {
+			for (int i = x; i < x + N; i++) {
+				for (int j = y; j < y + N; j++) arr[i][j] = ' ';
+			}
+			return;
+		}
+ 
+		if (N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+ 
+		int size = N / 3;
+		int count = 0;
+		for (int i = x; i < x + N; i += size) {
+			for (int j = y; j < y + N; j += size) {
+				count++;
+				if (count == 5) star(i, j, size, true);
+				else star(i, j, size, false);
+			}
+		}
 	}
 }
