8779fae7d4326a0f2bee96b5e2bdf3f2a818ff05
importqkk <import_qkk@naver.com>
2023-09-07 21:37:46 +0900

    230907

---
+++ CodingTest/src/baekjoon/q15652/Main.java
@@ -0,0 +1,38 @@
+package baekjoon.q15652;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int n, m;
+	public static int[] arr;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		arr = new int[m];
+		dfs(1, 0);
+		System.out.println(sb);
+		
+	}
+
+	private static void dfs(int at, int depth) {
+		if(depth == m) {
+			for(int val : arr) sb.append(val).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=at; i<=n; i++) {
+			arr[depth] = i;
+			dfs(i, depth + 1);
+		}
+	}
+}
