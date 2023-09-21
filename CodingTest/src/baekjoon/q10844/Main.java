d9f7b0e9e969dbde9a185062ec7d2975fa4a62b9
importqkk <import_qkk@naver.com>
2023-09-21 21:56:31 +0900

    230921

---
+++ CodingTest/src/baekjoon/q10844/Main.java
@@ -0,0 +1,33 @@
+package baekjoon.q10844;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	private static int mod = 1000000000;
+	private static int[][] dp;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		
+		dp = new int[n+1][10];
+		for(int i=1; i<=9; i++) dp[1][i] = 1;
+		
+		for(int i=2; i<=n; i++) {
+			for(int j=0; j<10; j++) {
+				if(j==0) dp[i][j] = dp[i-1][j+1] % mod;
+				else if(j==9) dp[i][j] = dp[i-1][j-1] % mod;
+				else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+			}
+		}
+		
+		long result = 0;
+		for(int i=0; i<10; i++) result += dp[n][i];
+		System.out.println(result % mod);
+		
+	}
+}
