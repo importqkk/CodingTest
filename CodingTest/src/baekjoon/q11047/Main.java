8e6a01fd6716090865ab6a47cf82f39542820443
importqkk <import_qkk@naver.com>
2023-10-04 22:08:35 +0900

    231004

---
+++ CodingTest/src/baekjoon/q11047/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.q11047;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) arr[i] = Integer.parseInt(br.readLine());
+		br.close();
+		
+		int cnt = 0;
+		for(int i=n-1; i>=0; i--) {
+			if(arr[i] <= k) {
+				cnt += k / arr[i];
+				k %= arr[i];
+			}
+		}
+		
+		System.out.println(cnt);
+		
+	}
+}
