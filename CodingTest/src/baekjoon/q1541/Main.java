40cc31e88ba1216212178ad7c34071074b8e408b
importqkk <import_qkk@naver.com>
2023-10-07 21:09:31 +0900

    231007

---
+++ CodingTest/src/baekjoon/q1541/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.q1541;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer minus = new StringTokenizer(br.readLine(), "-");
+		int ans = Integer.MAX_VALUE;
+		while (minus.hasMoreTokens()) {
+			int num = 0;
+			StringTokenizer plus = new StringTokenizer(minus.nextToken(), "+");
+			while(plus.hasMoreTokens()) num += Integer.parseInt(plus.nextToken());
+			if(ans == Integer.MAX_VALUE) ans = num;
+			else ans -= num;
+		}
+		br.close();
+		
+		System.out.println(ans);
+		
+	}
+}
