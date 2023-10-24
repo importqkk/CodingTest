fe58aee3f9138ba20d992b5509528acf4e3e887a
importqkk <import_qkk@naver.com>
2023-10-24 20:25:51 +0900

    231024

---
+++ CodingTest/src/baekjoon/q11279/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.q11279;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+		int x;
+		for(int i=0; i<n; i++) {
+			x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(heap.isEmpty()) sb.append(0).append("\n");
+				else sb.append(heap.poll()).append("\n");
+			} else heap.add(x);
+		}
+		br.close();
+		
+		System.out.println(sb.toString());
+		
+	}
+}
