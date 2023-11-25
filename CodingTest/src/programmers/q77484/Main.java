01a1e12b376d4b3e964d6bf095a592b7052fce61
importqkk <import_qkk@naver.com>
2023-11-25 21:31:20 +0900

    231125

--- CodingTest/src/programmers/q77484/Main.java
+++ CodingTest/src/programmers/q77484/Main.java
@@ -1,7 +1,4 @@
 package programmers.q77484;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 // Lv.1 로또의 최고 순위와 최저 순위
 // https://school.programmers.co.kr/learn/courses/30/lessons/77484
@@ -10,9 +7,6 @@ public class Main {
 	    public static int[] solution(int[] lottos, int[] win_nums) {
 	        int[] answer = new int[2];
 	        int zero = 0, cnt = 0;
-	        Map<Integer, Integer> map = new HashMap<>();
-	        map.put(6, 1); map.put(5, 2); map.put(4, 3); map.put(3, 4); map.put(2, 5);
-	        Arrays.sort(lottos); Arrays.sort(win_nums);
 	        for(int i=0; i<lottos.length; i++) {
 	        	if(lottos[i] == 0) {
 	        		zero++; continue;
@@ -21,10 +15,8 @@ public class Main {
 	        		if(lottos[i] == win_nums[j]) cnt++;
 	        	}
 	        }
-	        System.out.println("cnt: " + cnt);
-	        System.out.println("zero: " + zero);
-	        answer[0] = zero+cnt<2 ? 6 : map.get(zero+cnt);
-	        answer[1] = cnt<2 ? 6 : map.get(cnt);
+	        answer[0] = zero+cnt<2 ? 6 : 7-(zero+cnt);
+	        answer[1] = cnt<2 ? 6 : 7-cnt;
 	        return answer;
 	    }
 	}
