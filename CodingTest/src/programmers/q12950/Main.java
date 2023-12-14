538da69c1fc76c38e909d0c9418a34450f9f611e
importqkk <import_qkk@naver.com>
2023-12-14 21:41:08 +0900

    231214

---
+++ CodingTest/src/programmers/q12950/Main.java
@@ -0,0 +1,19 @@
+package programmers.q12950;
+
+// 행렬의 덧셈
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+public class Main {
+	class Solution {
+	    public static int[][] solution(int[][] arr1, int[][] arr2) {
+	        int[][] answer = new int[arr1.length][arr1[0].length];
+	        for(int i=0; i<arr1.length; i++) for(int j=0; j<arr1[0].length; j++) answer[i][j]=arr1[i][j]+arr2[i][j];
+	        return answer;
+	    }
+	}
+	public static void main(String[] args) {
+		int[][] arr1 = {{1, 2}, {2, 3}};
+		int[][] arr2 = {{3, 4}, {5, 6}};
+		int[][] result = Solution.solution(arr1, arr2);
+		for(int i=0; i<result.length; i++) for(int j=0; j<result[i].length; j++) System.out.println(result[i][j]);
+	}
+}
