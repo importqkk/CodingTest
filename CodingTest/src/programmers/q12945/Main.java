c9c85508f255c2f85278c612d8e603a411ae0cfa
importqkk <import_qkk@naver.com>
2024-01-23 20:20:13 +0900

    240123

---
+++ CodingTest/src/programmers/q12945/Main.java
@@ -0,0 +1,19 @@
+package programmers.q12945;
+
+// Lv.2 피보나치 수
+// https://school.programmers.co.kr/learn/courses/30/lessons/12945
+public class Main {
+	class Solution {
+	    public static int solution(int n) {
+	        int div = 1234567;
+	        int[] fibs = new int[n+1];
+	        fibs[0] = 0; fibs[1] = fibs[2] = 1;
+	        for(int i=3; i<fibs.length; i++) fibs[i] = (fibs[i-2] + fibs[i-1]) % div;
+	        return fibs[fibs.length-1];
+	    }
+	}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(Solution.solution(n));
+	}
+}
